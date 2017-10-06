cloudberrydb/cloudberrydb

commit 7e268107f31c305d74b532633ab146944ba40d52

Remove row order information from Flow.
A Motion node often needs to "merge" the incoming streams, to preserve the
overall sort order. Instead of carrying sort order information throughout
the later stages of planning, in the Flow struct, pass it as argument
directly to make_motion() and other functions, where a Motion node is
created. This simplifies things.

To make that work, we can no longer rely on apply_motion() to add the final
Motion on top of the plan, when the (sub-)query contains an ORDER BY. That's
because we no longer have that information available at apply_motion(). Add
the Motion node in grouping_planner() instead, where we still have that
information, as a path key.

When I started to work on this, this also fixed a bug, where the sortColIdx
of plan flow node may refer to wrong resno. A test case for that is
included. However, that case was since fixed by other coincidental changes
to partition elimination, so now this is just refactoring.

src/backend/optimizer/plan/createplan.c
@@ -4890,102 +4890,33 @@ make_sort_from_groupcols(PlannerInfo *root,
  *	  into the sorting list.
  */
 Sort *
-make_sort_from_reordered_groupcols(PlannerInfo *root,
-								   List *groupcls,
-								   AttrNumber *orig_grpColIdx,
-								   AttrNumber *new_grpColIdx,
-								   TargetEntry *grouping,
-								   TargetEntry *groupid,
-								   int req_ngrpkeys,
-								   Plan *lefttree)
+make_sort_from_pathkeys_and_groupingcol(PlannerInfo *root,
+										Plan *lefttree,
+										List *pathkeys,
+										TargetEntry *grouping,
+										TargetEntry *groupid)
 {
-	List	   *sub_tlist = lefttree->targetlist;
-	int			grpno = 0;
-	int			numgrpkeys,
-				numsortkeys;
-	AttrNumber *sortColIdx;
-	Oid		   *sortOperators;
-	bool	   *nullsFirst;
-	List	   *flat_groupcls;
+	Sort	   *sort;
+	Oid			sort_op;
 
-	/*
-	 * We will need at most list_length(groupcls) sort columns; possibly less
-	 */
-	numgrpkeys = num_distcols_in_grouplist(groupcls);
+	sort = make_sort_from_pathkeys(root, lefttree, pathkeys, -1, false);
 
-	Assert(req_ngrpkeys <= numgrpkeys);
+	sort->sortColIdx = repalloc(sort->sortColIdx, (sort->numCols + 2) * sizeof(AttrNumber));
+	sort->sortOperators = repalloc(sort->sortOperators, (sort->numCols + 2) * sizeof(Oid));
+	sort->nullsFirst = repalloc(sort->nullsFirst, (sort->numCols + 2) * sizeof(bool));
 
-	if (grouping != NULL)
-	{
-		Assert(groupid != NULL);
+	sort_op = ordering_oper_opid(exprType((Node *) grouping->expr));
 
-		sortColIdx = (AttrNumber *) palloc((numgrpkeys + 2) * sizeof(AttrNumber));
-		sortOperators = (Oid *) palloc((numgrpkeys + 2) * sizeof(Oid));
-		nullsFirst = (bool *) palloc((numgrpkeys + 2) * sizeof(bool));
-	}
-	else
-	{
-		sortColIdx = (AttrNumber *) palloc(numgrpkeys * sizeof(AttrNumber));
-		sortOperators = (Oid *) palloc(numgrpkeys * sizeof(Oid));
-		nullsFirst = (bool *) palloc(numgrpkeys * sizeof(bool));
-	}
+	sort->numCols = add_sort_column(grouping->resno, sort_op, false,
+								  sort->numCols,
+								  sort->sortColIdx, sort->sortOperators, sort->nullsFirst);
 
-	numsortkeys = 0;
+	sort_op = ordering_oper_opid(exprType((Node *) groupid->expr));
 
-	flat_groupcls = flatten_grouping_list(groupcls);
-
-	for (grpno = 0; grpno < req_ngrpkeys; grpno++)
-	{
-		GroupClause *grpcl;
-		TargetEntry *tle;
-		int			pos;
-
-		/*
-		 * Find the index position in orig_grpColIdx, in which the element is
-		 * equal to new_grpColIdx[grpno]. This index position points to the
-		 * place that the corresponding GroupClause in flat_groupcls.
-		 */
-		for (pos = 0; pos < numgrpkeys; pos++)
-		{
-			if (orig_grpColIdx[pos] == new_grpColIdx[grpno])
-				break;
-		}
-
-		Assert(pos < numgrpkeys);
-
-		grpcl = (GroupClause *) list_nth(flat_groupcls, pos);
-		tle = get_tle_by_resno(sub_tlist, new_grpColIdx[grpno]);
-
-		/*
-		 * Check for the possibility of duplicate group-by clauses --- the
-		 * parser should have removed 'em, but no point in sorting
-		 * redundantly.
-		 */
-		numsortkeys = add_sort_column(tle->resno, grpcl->sortop, false,
-									  numsortkeys,
-									  sortColIdx, sortOperators, nullsFirst);
-	}
-
-	if (grouping != NULL)
-	{
-		Oid			sort_op = ordering_oper_opid(exprType((Node *) grouping->expr));
-
-		numsortkeys = add_sort_column(grouping->resno, sort_op, false,
-									  numsortkeys,
-									  sortColIdx, sortOperators, nullsFirst);
-
-		sort_op = ordering_oper_opid(exprType((Node *) groupid->expr));
-
-		numsortkeys = add_sort_column(groupid->resno, sort_op, false,
-									  numsortkeys,
-									  sortColIdx, sortOperators, nullsFirst);
-	}
-
-
-	Assert(numsortkeys > 0);
-
-	return make_sort(root, lefttree, numsortkeys,
-					 sortColIdx, sortOperators, nullsFirst, -1.0);
+	sort->numCols = add_sort_column(groupid->resno, sort_op, false,
+								  sort->numCols,
+								  sort->sortColIdx, sort->sortOperators, sort->nullsFirst);
+	return sort;
 }
 
 /*
@@ -5023,6 +4954,75 @@ reconstruct_group_clause(List *orig_groupClause, List *tlist, AttrNumber *grpCol
 	}
 
 	return new_groupClause;
+}
+
+/* --------------------------------------------------------------------
+ * make_motion -- creates a Motion node.
+ * Caller must have built the pHashDefn, pFixedDefn,
+ * and pSortDefn structs already.
+ * useExecutorVarFormat is true if make_motion is called after setrefs
+ * This call only make a motion node, without filling in flow info
+ * After calling this function, caller need to call add_slice_to_motion
+ * --------------------------------------------------------------------
+ */
+Motion *
+make_motion(PlannerInfo *root, Plan *lefttree, List *sortPathKeys, bool useExecutorVarFormat)
+{
+    Motion *node = makeNode(Motion);
+    Plan   *plan = &node->plan;
+
+	Assert(lefttree);
+	Assert(!IsA(lefttree, Motion));
+
+	plan->startup_cost = lefttree->startup_cost;
+	plan->total_cost = lefttree->total_cost;
+	plan->plan_rows = lefttree->plan_rows;
+	plan->plan_width = lefttree->plan_width;
+
+	plan->targetlist = cdbpullup_targetlist(lefttree, useExecutorVarFormat);
+	plan->qual = NIL;
+	plan->lefttree = lefttree;
+	plan->righttree = NULL;
+	plan->dispatch = DISPATCH_PARALLEL;
+
+	if (sortPathKeys)
+	{
+		Sort *sort;
+
+		/* FIXME: add_keys_to_targetlist, or not? */
+		sort = make_sort_from_pathkeys(root, lefttree, sortPathKeys, -1, true);
+
+		node->numSortCols = sort->numCols;
+		node->sortColIdx = sort->sortColIdx;
+		node->sortOperators = sort->sortOperators;
+		node->nullsFirst = sort->nullsFirst;
+
+#ifdef USE_ASSERT_CHECKING
+		/*
+		 * If the child node was a Sort, then surely the order the caller gave us
+		 * must match that of the underlying sort.
+		 */
+		if (IsA(lefttree, Sort))
+		{
+			Sort	   *childsort = (Sort *) lefttree;
+			Assert(childsort->numCols >= node->numSortCols);
+			Assert(memcmp(childsort->sortColIdx, node->sortColIdx, node->numSortCols * sizeof(AttrNumber)) == 0);
+			Assert(memcmp(childsort->sortOperators, node->sortOperators, node->numSortCols * sizeof(Oid)) == 0);
+			Assert(memcmp(childsort->nullsFirst, node->nullsFirst, node->numSortCols * sizeof(bool)) == 0);
+		}
+#endif
+	}
+	node->sendSorted = (node->numSortCols > 0);
+
+	plan->extParam = bms_copy(lefttree->extParam);
+	plan->allParam = bms_copy(lefttree->allParam);
+
+	plan->flow = NULL;
+
+	node->outputSegIdx = NULL;
+	node->numOutputSegs = 0;
+
+	return node;
 }
 
 Material *
@@ -5658,7 +5658,7 @@ plan_pushdown_tlist(PlannerInfo *root, Plan *plan, List *tlist)
 		/* Fix up annotation of plan's distribution and ordering properties. */
 		if (plan->flow)
 			plan->flow = pull_up_Flow((Plan *) make_result(root, tlist, NULL, plan),
-									  plan, true);
+									  plan);
 
 		/* Install the new targetlist. */
 		plan->targetlist = tlist;
@@ -5670,9 +5670,9 @@ plan_pushdown_tlist(PlannerInfo *root, Plan *plan, List *tlist)
 		/* Insert a Result node to evaluate the targetlist. */
 		plan = (Plan *) make_result(root, tlist, NULL, subplan);
 
-		/* Propagate the subplan's distribution and ordering properties. */
+		/* Propagate the subplan's distribution. */
 		if (subplan->flow)
-			plan->flow = pull_up_Flow(plan, subplan, true);
+			plan->flow = pull_up_Flow(plan, subplan);
 	}
 	return plan;
 }	/* plan_pushdown_tlist */

hlinnaka/gpdb

commit 6fc2d73d374d7933736dcb2c91581bb7f5aad323

Fix crash with a ROLLUP query.
This was broken by commit 7e268107f3, which refactored the code that deals
with path keys and sorts in plangroupext.c. The new function
make_sort_from_pathkeys_and_groupingcol(), which replaced the old
make_sort_from_reordered_groupcols() function, didn't work quite the same
as the old function. I'm not sure what exactly went wrong there, but the
caller already has the column number and operator information at hand, so
we can use it to construct the Sort directly, without trying to re-find the
original target list entries of the sort columns.

Commit 7e268107f3 also neglected the comments in
make_sort_from_pathkeys_and_groupingcol(), but this commit removes the whole
function.

Fixes github issue #3447.

src/backend/optimizer/plan/createplan.c
@@ -166,9 +166,6 @@ static WorkTableScan *make_worktablescan(List *qptlist, List *qpqual,
 										 Index scanrelid, int wtParam);
 static BitmapAnd *make_bitmap_and(List *bitmapplans);
 static BitmapOr *make_bitmap_or(List *bitmapplans);
-static Sort *make_sort(PlannerInfo *root, Plan *lefttree, int numCols,
-		  AttrNumber *sortColIdx, Oid *sortOperators, bool *nullsFirst,
-		  double limit_tuples);
 static List *flatten_grouping_list(List *groupcls);
 
 
@@ -4434,7 +4431,7 @@ make_mergejoin(List *tlist,
  * arrays already.	limit_tuples is as for cost_sort (in particular, pass
  * -1 if no limit)
  */
-static Sort *
+Sort *
 make_sort(PlannerInfo *root, Plan *lefttree, int numCols,
 		  AttrNumber *sortColIdx, Oid *sortOperators, bool *nullsFirst,
 		  double limit_tuples)
@@ -4882,48 +4879,6 @@ make_sort_from_groupcols(PlannerInfo *root,
 
 	return make_sort(root, lefttree, numsortkeys,
 					 sortColIdx, sortOperators, nullsFirst, -1.0);
-}
-
-
-/*
- * make_sort_from_reordered_groupcols
- *	  Create sort plan to sort based on re-ordered grouping columns.
- *
- * 'groupcls' is the list of GroupClauses
- * 'orig_groupColIdx' is the original columns numbers for groupcls
- * 'new_grpColIdx' gives the re-ordered column numbers to use
- * 'grouping' represents the target entry for the Grouping column.
- *	  If this value is not NULL, "Grouping" column will be added
- *	  into the sorting list.
- */
-Sort *
-make_sort_from_pathkeys_and_groupingcol(PlannerInfo *root,
-										Plan *lefttree,
-										List *pathkeys,
-										TargetEntry *grouping,
-										TargetEntry *groupid)
-{
-	Sort	   *sort;
-	Oid			sort_op;
-
-	sort = make_sort_from_pathkeys(root, lefttree, pathkeys, -1, false);
-
-	sort->sortColIdx = repalloc(sort->sortColIdx, (sort->numCols + 2) * sizeof(AttrNumber));
-	sort->sortOperators = repalloc(sort->sortOperators, (sort->numCols + 2) * sizeof(Oid));
-	sort->nullsFirst = repalloc(sort->nullsFirst, (sort->numCols + 2) * sizeof(bool));
-
-	sort_op = ordering_oper_opid(exprType((Node *) grouping->expr));
-
-	sort->numCols = add_sort_column(grouping->resno, sort_op, false,
-								  sort->numCols,
-								  sort->sortColIdx, sort->sortOperators, sort->nullsFirst);
-
-	sort_op = ordering_oper_opid(exprType((Node *) groupid->expr));
-
-	sort->numCols = add_sort_column(groupid->resno, sort_op, false,
-								  sort->numCols,
-								  sort->sortColIdx, sort->sortOperators, sort->nullsFirst);
-	return sort;
 }
 
 /*

src/backend/optimizer/plan/plangroupext.c
@@ -667,9 +667,9 @@ make_list_aggs_for_rollup(PlannerInfo *root,
 			/* Add sort node if needed, and set AggStrategy */
 			if (root->parse->groupClause)
 			{
-				group_pathkeys =  reorder_pathkeys(root, root->group_pathkeys,
-												   orig_grpColIdx,
-												   groupColIdx);
+				group_pathkeys = reorder_pathkeys(root, root->group_pathkeys,
+												  orig_grpColIdx,
+												  groupColIdx);
 				if (!pathkeys_contained_in(group_pathkeys,
 										   context->current_pathkeys))
 				{
@@ -890,36 +890,23 @@ make_list_aggs_for_rollup(PlannerInfo *root,
 		/* Add a sort node */
 		if (context->aggstrategy == AGG_SORTED)
 		{
-			AttrNumber *orig_prelimGroupColIdx =
-				(AttrNumber *)palloc0(context->numGroupCols * sizeof(AttrNumber));
-			int attno;
-			TargetEntry *grouping_entry;
-			TargetEntry *groupid_entry;
+			Oid		   *cmpOperators = palloc(context->numGroupCols * sizeof(Oid));
+			bool	   *nullsFirst = palloc(context->numGroupCols * sizeof(bool));
+			int			i;
 
-			/* Create a group column index array that maps columns in
-			 * prelimGroupColIdx to the position in the original GROUP
-			 * BY clause.
-			 */
-			for (attno=0; attno<context->numGroupCols -1; attno++)
-				orig_prelimGroupColIdx[attno] = attno+1;
+			for (i = 0; i < context->numGroupCols; i++)
+			{
+				cmpOperators[i] = get_ordering_op_for_equality_op(prelimGroupOperators[i], false);
+				nullsFirst[i] = false;
+			}
 
-			group_pathkeys =  reorder_pathkeys(root, root->group_pathkeys,
-											   orig_prelimGroupColIdx,
-											   prelimGroupColIdx);
-
-			grouping_entry =
-				get_tle_by_resno(agg_node->targetlist,
-								 prelimGroupColIdx[context->numGroupCols - 2]);
-			groupid_entry =
-				get_tle_by_resno(agg_node->targetlist,
-								 prelimGroupColIdx[context->numGroupCols - 1]);
-			agg_node = (Plan *)
-				make_sort_from_pathkeys_and_groupingcol(root, agg_node, group_pathkeys,
-														grouping_entry, groupid_entry);
-			pfree(orig_prelimGroupColIdx);
+			agg_node = (Plan *) make_sort(root, agg_node, context->numGroupCols,
+										  prelimGroupColIdx,
+										  cmpOperators,
+										  nullsFirst, -1.0);
 			mark_sort_locus(agg_node);
 		}
-		
+
 		/* Convert # groups to long int --- but 'ware overflow! */
 		lNumGroups = (long) Min(*context->p_dNumGroups, (double) LONG_MAX);
 

src/include/optimizer/planmain.h
@@ -141,15 +141,13 @@ extern Sort *make_sort_from_sortclauses(PlannerInfo *root, List *sortcls,
 extern Sort *make_sort_from_groupcols(PlannerInfo *root, List *groupcls,
 									  AttrNumber *grpColIdx, bool appendGrouping,
 									  Plan *lefttree);
-extern Sort *make_sort_from_pathkeys_and_groupingcol(PlannerInfo *root,
-										Plan *lefttree,
-										List *pathkeys,
-										TargetEntry *grouping,
-										TargetEntry *groupid);
 extern List *reconstruct_group_clause(List *orig_groupClause, List *tlist,
 						 AttrNumber *grpColIdx, int numcols);
 
 extern Motion *make_motion(PlannerInfo *root, Plan *lefttree, List *sortPathKeys, bool useExecutorVarFormat);
+extern Sort *make_sort(PlannerInfo *root, Plan *lefttree, int numCols,
+		  AttrNumber *sortColIdx, Oid *sortOperators, bool *nullsFirst,
+		  double limit_tuples);
 
 extern Agg *make_agg(PlannerInfo *root, List *tlist, List *qual,
 					 AggStrategy aggstrategy, bool streaming,

src/test/regress/expected/bfv_olap.out
@@ -1,8 +1,62 @@
+-- Tests for old bugs related to OLAP queries.
+-- First create a schema to contain the test tables, and few common test
+-- tables that are shared by several test queries.
+create schema bfv_olap;
+set search_path=bfv_olap;
+create table customer
+(
+  cn int not null,
+  cname text not null,
+  cloc text,
+  primary key (cn)
+) distributed by (cn);
+NOTICE:  CREATE TABLE / PRIMARY KEY will create implicit index "customer_pkey" for table "customer"
+insert into customer values
+  ( 1, 'Macbeth', 'Inverness'),
+  ( 2, 'Duncan', 'Forres'),
+  ( 3, 'Lady Macbeth', 'Inverness'),
+  ( 4, 'Witches, Inc', 'Lonely Heath');
+create table vendor
+(
+  vn int not null,
+  vname text not null,
+  vloc text,
+  primary key (vn)
+) distributed by (vn);
+NOTICE:  CREATE TABLE / PRIMARY KEY will create implicit index "vendor_pkey" for table "vendor"
+insert into vendor values
+  ( 10, 'Witches, Inc', 'Lonely Heath'),
+  ( 20, 'Lady Macbeth', 'Inverness'),
+  ( 30, 'Duncan', 'Forres'),
+  ( 40, 'Macbeth', 'Inverness'),
+  ( 50, 'Macduff', 'Fife');
+create table sale
+(
+  cn int not null,
+  vn int not null,
+  pn int not null,
+  dt date not null,
+  qty int not null,
+  prc float not null,
+  primary key (cn, vn, pn)
+) distributed by (cn,vn,pn);
+NOTICE:  CREATE TABLE / PRIMARY KEY will create implicit index "sale_pkey" for table "sale"
+insert into sale values
+  ( 2, 40, 100, '1401-1-1', 1100, 2400),
+  ( 1, 10, 200, '1401-3-1', 1, 0),
+  ( 3, 40, 200, '1401-4-1', 1, 0),
+  ( 1, 20, 100, '1401-5-1', 1, 0),
+  ( 1, 30, 300, '1401-5-2', 1, 0),
+  ( 1, 50, 400, '1401-6-1', 1, 0),
+  ( 2, 50, 400, '1401-6-1', 1, 0),
+  ( 1, 30, 500, '1401-6-1', 12, 5),
+  ( 3, 30, 500, '1401-6-1', 12, 5),
+  ( 3, 30, 600, '1401-6-1', 12, 5),
+  ( 4, 40, 700, '1401-6-1', 1, 1),
+  ( 4, 40, 800, '1401-6-1', 1, 1);
 --
 -- Test case errors out when we define aggregates without preliminary functions and use it as an aggregate derived window function.
 --
-create schema bfv_olap;
-set search_path=bfv_olap;
 -- SETUP
 -- start_ignore
 drop table if exists toy;
@@ -355,50 +409,6 @@ drop table if exists t;
 --
 -- Passing through distribution matching type in default implementation
 --
--- SETUP
--- start_ignore
-drop table if exists customer;
-NOTICE:  table "customer" does not exist, skipping
-drop table if exists sale;
-NOTICE:  table "sale" does not exist, skipping
--- end_ignore
-create table customer
-(
-	cn int not null,
-	cname text not null,
-	cloc text,
-	primary key (cn)
-) distributed by (cn);
-NOTICE:  CREATE TABLE / PRIMARY KEY will create implicit index "customer_pkey" for table "customer"
-insert into customer values
-  ( 1, 'Macbeth', 'Inverness'),
-  ( 2, 'Duncan', 'Forres'),
-  ( 3, 'Lady Macbeth', 'Inverness'),
-  ( 4, 'Witches, Inc', 'Lonely Heath');
-create table sale
-(
-	cn int not null,
-	vn int not null,
-	pn int not null,
-	dt date not null,
-	qty int not null,
-	prc float not null,
-	primary key (cn, vn, pn)
-) distributed by (cn,vn,pn);
-NOTICE:  CREATE TABLE / PRIMARY KEY will create implicit index "sale_pkey" for table "sale"
-insert into sale values
-  ( 2, 40, 100, '1401-1-1', 1100, 2400),
-  ( 1, 10, 200, '1401-3-1', 1, 0),
-  ( 3, 40, 200, '1401-4-1', 1, 0),
-  ( 1, 20, 100, '1401-5-1', 1, 0),
-  ( 1, 30, 300, '1401-5-2', 1, 0),
-  ( 1, 50, 400, '1401-6-1', 1, 0),
-  ( 2, 50, 400, '1401-6-1', 1, 0),
-  ( 1, 30, 500, '1401-6-1', 12, 5),
-  ( 3, 30, 500, '1401-6-1', 12, 5),
-  ( 3, 30, 600, '1401-6-1', 12, 5),
-  ( 4, 40, 700, '1401-6-1', 1, 1),
-  ( 4, 40, 800, '1401-6-1', 1, 1);
   
 -- TEST
 select cname,
@@ -422,13 +432,8 @@ order by 1, 2;
  Witches, Inc |    1
 (12 rows)
 
--- CLEANUP
--- start_ignore
-drop table if exists customer;
-drop table if exists sale;
--- end_ignore
 --
--- Optimzier query crashing for logical window with no window functions
+-- Optimizer query crashing for logical window with no window functions
 --
 -- SETUP
 create table mpp23240(a int, b int, c int, d int, e int, f int);
@@ -452,9 +457,10 @@ from (select * from mpp23240 where f > 10) x;
 -- CLEANUP
 -- start_ignore
 drop table mpp23240;
-drop schema if exists bfv_olap;
 -- end_ignore
+--
 -- Test for the bug reported at https://github.com/greenplum-db/gpdb/issues/2236
+--
 create table test1 (x int, y int, z double precision);
 insert into test1 select a, b, a*10 + b from generate_series(1, 5) a, generate_series(1, 5) b;
 select sum(z) over (partition by x) as sumx, sum(z) over (partition by y) as sumy from test1;
@@ -488,8 +494,10 @@ select sum(z) over (partition by x) as sumx, sum(z) over (partition by y) as sum
 (25 rows)
 
 drop table test1;
+--
 -- This failed at one point because of an over-zealous syntax check, with
 -- "window functions not allowed in WHERE clause" error.
+--
 select sum(g) from generate_series(1, 5) g
 where g in (
   select rank() over (order by x) from generate_series(1,5) x
@@ -498,6 +506,65 @@ where g in (
 -----
   15
 (1 row)
+
+--
+-- This caused a crash in ROLLUP planning at one point.
+--
+SELECT sale.vn
+FROM sale,vendor
+WHERE sale.vn=vendor.vn
+GROUP BY ROLLUP( (sale.dt,sale.cn),(sale.pn),(sale.vn));
+ vn 
+----
+ 40
+   
+   
+ 20
+   
+   
+ 30
+   
+   
+   
+ 10
+ 40
+   
+   
+ 50
+   
+   
+ 30
+   
+   
+ 40
+   
+   
+   
+   
+   
+   
+ 30
+   
+ 50
+   
+ 30
+ 40
+   
+(34 rows)
+
+SELECT DISTINCT sale.vn
+FROM sale,vendor
+WHERE sale.vn=vendor.vn
+GROUP BY ROLLUP( (sale.dt,sale.cn),(sale.pn),(sale.vn));
+ vn 
+----
+ 10
+ 20
+ 30
+ 40
+ 50
+   
+(6 rows)
 
 -- CLEANUP
 -- start_ignore

src/test/regress/sql/bfv_olap.sql
@@ -1,9 +1,74 @@
+-- Tests for old bugs related to OLAP queries.
+
+-- First create a schema to contain the test tables, and few common test
+-- tables that are shared by several test queries.
+create schema bfv_olap;
+set search_path=bfv_olap;
+
+create table customer
+(
+  cn int not null,
+  cname text not null,
+  cloc text,
+
+  primary key (cn)
+
+) distributed by (cn);
+
+insert into customer values
+  ( 1, 'Macbeth', 'Inverness'),
+  ( 2, 'Duncan', 'Forres'),
+  ( 3, 'Lady Macbeth', 'Inverness'),
+  ( 4, 'Witches, Inc', 'Lonely Heath');
+
+create table vendor
+(
+  vn int not null,
+  vname text not null,
+  vloc text,
+
+  primary key (vn)
+
+) distributed by (vn);
+
+insert into vendor values
+  ( 10, 'Witches, Inc', 'Lonely Heath'),
+  ( 20, 'Lady Macbeth', 'Inverness'),
+  ( 30, 'Duncan', 'Forres'),
+  ( 40, 'Macbeth', 'Inverness'),
+  ( 50, 'Macduff', 'Fife');
+
+create table sale
+(
+  cn int not null,
+  vn int not null,
+  pn int not null,
+  dt date not null,
+  qty int not null,
+  prc float not null,
+
+  primary key (cn, vn, pn)
+
+) distributed by (cn,vn,pn);
+
+insert into sale values
+  ( 2, 40, 100, '1401-1-1', 1100, 2400),
+  ( 1, 10, 200, '1401-3-1', 1, 0),
+  ( 3, 40, 200, '1401-4-1', 1, 0),
+  ( 1, 20, 100, '1401-5-1', 1, 0),
+  ( 1, 30, 300, '1401-5-2', 1, 0),
+  ( 1, 50, 400, '1401-6-1', 1, 0),
+  ( 2, 50, 400, '1401-6-1', 1, 0),
+  ( 1, 30, 500, '1401-6-1', 12, 5),
+  ( 3, 30, 500, '1401-6-1', 12, 5),
+  ( 3, 30, 600, '1401-6-1', 12, 5),
+  ( 4, 40, 700, '1401-6-1', 1, 1),
+  ( 4, 40, 800, '1401-6-1', 1, 1);
+
+
 --
 -- Test case errors out when we define aggregates without preliminary functions and use it as an aggregate derived window function.
 --
-
-create schema bfv_olap;
-set search_path=bfv_olap;
 
 -- SETUP
 -- start_ignore
@@ -325,57 +390,6 @@ drop table if exists t;
 --
 -- Passing through distribution matching type in default implementation
 --
-
--- SETUP
--- start_ignore
-drop table if exists customer;
-drop table if exists sale;
--- end_ignore
-create table customer
-(
-	cn int not null,
-	cname text not null,
-	cloc text,
-
-	primary key (cn)
-
-) distributed by (cn);
-
-
-insert into customer values
-  ( 1, 'Macbeth', 'Inverness'),
-  ( 2, 'Duncan', 'Forres'),
-  ( 3, 'Lady Macbeth', 'Inverness'),
-  ( 4, 'Witches, Inc', 'Lonely Heath');
-
-
-create table sale
-(
-	cn int not null,
-	vn int not null,
-	pn int not null,
-	dt date not null,
-	qty int not null,
-	prc float not null,
-
-	primary key (cn, vn, pn)
-
-) distributed by (cn,vn,pn);
-
-
-insert into sale values
-  ( 2, 40, 100, '1401-1-1', 1100, 2400),
-  ( 1, 10, 200, '1401-3-1', 1, 0),
-  ( 3, 40, 200, '1401-4-1', 1, 0),
-  ( 1, 20, 100, '1401-5-1', 1, 0),
-  ( 1, 30, 300, '1401-5-2', 1, 0),
-  ( 1, 50, 400, '1401-6-1', 1, 0),
-  ( 2, 50, 400, '1401-6-1', 1, 0),
-  ( 1, 30, 500, '1401-6-1', 12, 5),
-  ( 3, 30, 500, '1401-6-1', 12, 5),
-  ( 3, 30, 600, '1401-6-1', 12, 5),
-  ( 4, 40, 700, '1401-6-1', 1, 1),
-  ( 4, 40, 800, '1401-6-1', 1, 1);
   
 -- TEST
 select cname,
@@ -384,15 +398,9 @@ from sale, customer
 where sale.cn = customer.cn
 order by 1, 2;
 
--- CLEANUP
--- start_ignore
-drop table if exists customer;
-drop table if exists sale;
--- end_ignore
-
 
 --
--- Optimzier query crashing for logical window with no window functions
+-- Optimizer query crashing for logical window with no window functions
 --
 
 -- SETUP
@@ -415,7 +423,10 @@ from (select * from mpp23240 where f > 10) x;
 drop table mpp23240;
 -- end_ignore
 
+
+--
 -- Test for the bug reported at https://github.com/greenplum-db/gpdb/issues/2236
+--
 create table test1 (x int, y int, z double precision);
 insert into test1 select a, b, a*10 + b from generate_series(1, 5) a, generate_series(1, 5) b;
 
@@ -423,12 +434,29 @@ select sum(z) over (partition by x) as sumx, sum(z) over (partition by y) as sum
 
 drop table test1;
 
+
+--
 -- This failed at one point because of an over-zealous syntax check, with
 -- "window functions not allowed in WHERE clause" error.
+--
 select sum(g) from generate_series(1, 5) g
 where g in (
   select rank() over (order by x) from generate_series(1,5) x
 );
+
+
+--
+-- This caused a crash in ROLLUP planning at one point.
+--
+SELECT sale.vn
+FROM sale,vendor
+WHERE sale.vn=vendor.vn
+GROUP BY ROLLUP( (sale.dt,sale.cn),(sale.pn),(sale.vn));
+
+SELECT DISTINCT sale.vn
+FROM sale,vendor
+WHERE sale.vn=vendor.vn
+GROUP BY ROLLUP( (sale.dt,sale.cn),(sale.pn),(sale.vn));
 
 
 -- CLEANUP