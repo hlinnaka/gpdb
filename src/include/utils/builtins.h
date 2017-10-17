4fedc6ef1e00fd18bc3296ed035ccac066862f37
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-10-17 17:58:50 +0300

    Remove built-in stub functions for QuickLZ compressor.
    The proprietary build can install them as normal C language functions,
    with CREATE FUNCTION, instead.
    
    In the passing, remove some unused QuickLZ debugging GUCs.
    
    This doesn't yet get rid of all references to QuickLZ, unfortunately. The
    GUC and reloption validation code still needs to know about it, so that
    they can validate the options read from postgresql.conf, when starting up
    postmaster. For the same reason, you cannot yet add custom compression
    algorithms, besides quicklz, as an extension. But this is another step in
    the right direction, anyway.

--- src/include/utils/builtins.h
+++ src/include/utils/builtins.h
@@ -1224,12 +1224,6 @@ extern Datum gp_delete_relation_node_entry(PG_FUNCTION_ARGS);
 extern Datum gp_persistent_relation_node_check(PG_FUNCTION_ARGS);
 
 /* storage/compress.c */
-extern Datum quicklz_constructor(PG_FUNCTION_ARGS);
-extern Datum quicklz_destructor(PG_FUNCTION_ARGS);
-extern Datum quicklz_compress(PG_FUNCTION_ARGS);
-extern Datum quicklz_decompress(PG_FUNCTION_ARGS);
-extern Datum quicklz_validator(PG_FUNCTION_ARGS);
-
 extern Datum zlib_constructor(PG_FUNCTION_ARGS);
 extern Datum zlib_destructor(PG_FUNCTION_ARGS);
 extern Datum zlib_compress(PG_FUNCTION_ARGS);
@@ -1262,7 +1256,6 @@ extern Datum dummy_compression_validator(PG_FUNCTION_ARGS);
 
 extern Datum gp_compressor(PG_FUNCTION_ARGS);
 extern Datum gp_decompressor(PG_FUNCTION_ARGS);
-extern Datum test_quicklz_compression(PG_FUNCTION_ARGS);
 
 /* percentile.c */
 extern Datum percentile_cont_trans(PG_FUNCTION_ARGS);
