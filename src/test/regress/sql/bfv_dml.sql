cloudberrydb/cloudberrydb

commit 740f304eeabee7c5ab9dc82f3db5e48b3f3309d2

Fix confusion on different kinds of "tuples".
The code in ExecInsert, ExecUpdate, and CopyFrom was confused on what kind
of a tuple the "tuple" variable might hold at different times. In
particular, if you had a trigger on an append-only table, they would pass
a MemTuple to Exec*Triggers() functions, which expect a HeapTuple.

To fix, refactor the code so that it's always clear what kind of a tuple
we're dealing with. The compiler will now throw warnings if they are
conflated.

We cannot, in fact, support ON UPDATE or ON DELETE triggers on AO tables
in a sane way. GetTupleForTrigger() is hopelessly heap-specific. We could
perhaps change it to do a lookup in the append only table's visibility map
instead of looking at a heap tuple's xmin/xmax, but looking up the original
tuple in an AO table would be fairly expensive anyway. As far as I can see,
that never worked correctly, but let's add a check in CREATE TRIGGER to
forbid that.

ON INSERT triggers now work, also on AOCS tables. There was previously
checks to throw an error if an AOCS table had a trigger, but I see no
reason to forbid that particular case.

Fixes github issue #3680.

src/backend/executor/execMain.c
@@ -3071,18 +3071,15 @@ ExecInsert(TupleTableSlot *slot,
 		   PlanGenerator planGen,
 		   bool isUpdate)
 {
-	void	   *tuple;
 	ResultRelInfo *resultRelInfo;
 	Relation	resultRelationDesc;
 	Oid			newId;
-	TupleTableSlot *partslot = NULL;
-
-	AOTupleId	aoTupleId = AOTUPLEID_INIT;
 
 	bool		rel_is_heap = false;
 	bool 		rel_is_aorows = false;
 	bool		rel_is_aocols = false;
 	bool		rel_is_external = false;
+	ItemPointerData lastTid;
 
 	/*
 	 * get information on the (current) result relation
@@ -3147,57 +3144,33 @@ ExecInsert(TupleTableSlot *slot,
 	rel_is_aorows = RelationIsAoRows(resultRelationDesc);
 	rel_is_external = RelationIsExternal(resultRelationDesc);
 
-	partslot = reconstructMatchingTupleSlot(slot, resultRelInfo);
-	if (rel_is_heap)
+	slot = reconstructMatchingTupleSlot(slot, resultRelInfo);
+
+	if (rel_is_external &&
+		estate->es_result_partitions &&
+		estate->es_result_partitions->part->parrelid != 0)
 	{
-		tuple = ExecFetchSlotHeapTuple(partslot);
-	}
-	else if (rel_is_aorows)
-	{
-		tuple = ExecFetchSlotMemTuple(partslot, false);
-	}
-	else if (rel_is_external) 
-	{
-		if (estate->es_result_partitions && 
-			estate->es_result_partitions->part->parrelid != 0)
-		{
-			ereport(ERROR,
+		ereport(ERROR,
 				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-				errmsg("Insert into external partitions not supported.")));			
-			return;
-		}
-		else
-		{
-			/*
-			 * Make a modifiable copy, since external_insert() takes the
-			 * liberty to modify the tuple.
-			 */
-			tuple = ExecCopySlotHeapTuple(partslot);
-		}
-	}
-	else
-	{
-		Assert(rel_is_aocols);
-		tuple = ExecFetchSlotMemTuple(partslot, true);
+				 errmsg("Insert into external partitions not supported.")));
 	}
 
-	Assert(partslot != NULL && tuple != NULL);
+	Assert(slot != NULL);
 
 	/* BEFORE ROW INSERT Triggers */
 	if (resultRelInfo->ri_TrigDesc &&
 		resultRelInfo->ri_TrigDesc->n_before_row[TRIGGER_EVENT_INSERT] > 0)
 	{
 		HeapTuple	newtuple;
+		HeapTuple	tuple;
+
+		tuple = ExecFetchSlotHeapTuple(slot);
 
 		/*
 		 * Not implemented for ORCA yet. It should fall back to the Postgres planner
 		 * if there are any before-row triggers.
 		 */
 		Assert(planGen == PLANGEN_PLANNER);
-
-		/* NYI */
-		if (rel_is_aocols)
-			elog(ERROR, "triggers are not supported on tables that use column-oriented storage");
 
 		newtuple = ExecBRInsertTriggers(estate, resultRelInfo, tuple);
 
@@ -3214,12 +3187,12 @@ ExecInsert(TupleTableSlot *slot,
 			 */
 			TupleTableSlot *newslot = estate->es_trig_tuple_slot;
 
-			if (newslot->tts_tupleDescriptor != partslot->tts_tupleDescriptor)
-				ExecSetSlotDescriptor(newslot, partslot->tts_tupleDescriptor);
+			if (newslot->tts_tupleDescriptor != slot->tts_tupleDescriptor)
+				ExecSetSlotDescriptor(newslot, slot->tts_tupleDescriptor);
 			ExecStoreHeapTuple(newtuple, newslot, InvalidBuffer, false);
-			newslot->tts_tableOid = partslot->tts_tableOid; /* for constraints */
+			newslot->tts_tableOid = slot->tts_tableOid; /* for constraints */
+			slot = newslot;
 			tuple = newtuple;
-			partslot = newslot;
 		}
 	}
 
@@ -3227,7 +3200,7 @@ ExecInsert(TupleTableSlot *slot,
 	 * Check the constraints of the tuple
 	 */
 	if (resultRelationDesc->rd_att->constr)
-		ExecConstraints(resultRelInfo, partslot, estate);
+		ExecConstraints(resultRelInfo, slot, estate);
 
 	/*
 	 * insert the tuple
@@ -3239,6 +3212,8 @@ ExecInsert(TupleTableSlot *slot,
 	 */
 	if (rel_is_aorows)
 	{
+		MemTuple	mtuple;
+
 		if (resultRelInfo->ri_aoInsertDesc == NULL)
 		{
 			/* Set the pre-assigned fileseg number to insert into */
@@ -3251,7 +3226,8 @@ ExecInsert(TupleTableSlot *slot,
 
 		}
 
-		appendonly_insert(resultRelInfo->ri_aoInsertDesc, tuple, &newId, &aoTupleId);
+		mtuple = ExecFetchSlotMemTuple(slot, false);
+		appendonly_insert(resultRelInfo->ri_aoInsertDesc, mtuple, &newId, (AOTupleId *) &lastTid);
 	}
 	else if (rel_is_aocols)
 	{
@@ -3262,60 +3238,59 @@ ExecInsert(TupleTableSlot *slot,
 																resultRelInfo->ri_aosegno, false);
 		}
 
-		newId = aocs_insert(resultRelInfo->ri_aocsInsertDesc, partslot);
-		aoTupleId = *((AOTupleId*)slot_get_ctid(partslot));
+		newId = aocs_insert(resultRelInfo->ri_aocsInsertDesc, slot);
+		lastTid = *slot_get_ctid(slot);
 	}
 	else if (rel_is_external)
 	{
 		/* Writable external table */
+		HeapTuple tuple;
+
 		if (resultRelInfo->ri_extInsertDesc == NULL)
 			resultRelInfo->ri_extInsertDesc = external_insert_init(resultRelationDesc);
 
+		tuple = ExecFetchSlotHeapTuple(slot);
+
 		newId = external_insert(resultRelInfo->ri_extInsertDesc, tuple);
+		ItemPointerSetInvalid(&lastTid);
 	}
 	else
 	{
+		HeapTuple tuple;
+
 		Insist(rel_is_heap);
+
+		tuple = ExecFetchSlotHeapTuple(slot);
 
 		newId = heap_insert(resultRelationDesc,
 							tuple,
 							estate->es_output_cid,
 							true, true, GetCurrentTransactionId());
+		lastTid = tuple->t_self;
 	}
 
 	(estate->es_processed)++;
 	(resultRelInfo->ri_aoprocessed)++;
 	estate->es_lastoid = newId;
+	setLastTid(&lastTid);
 
-	partslot->tts_tableOid = RelationGetRelid(resultRelationDesc);
+	slot->tts_tableOid = RelationGetRelid(resultRelationDesc);
 
-	if (rel_is_aorows || rel_is_aocols)
+	/*
+	 * insert index entries for tuple
+	 */
+	if (resultRelInfo->ri_NumIndices > 0)
+		ExecInsertIndexTuples(slot, &lastTid, estate, false);
+
+	/* AFTER ROW INSERT Triggers */
+	if (resultRelInfo->ri_TrigDesc &&
+		resultRelInfo->ri_TrigDesc->n_after_row[TRIGGER_EVENT_INSERT] > 0)
 	{
-		/*
-		 * insert index entries for AO Row-Store tuple
-		 */
-		if (resultRelInfo->ri_NumIndices > 0)
-			ExecInsertIndexTuples(partslot, (ItemPointer)&aoTupleId, estate, false);
-	}
-	else
-	{
-		/* Use parttuple for index update in case this is an indexed heap table. */
-		TupleTableSlot *xslot = partslot;
-		void *xtuple = tuple;
+		HeapTuple tuple = ExecFetchSlotHeapTuple(slot);
 
-		setLastTid(&(((HeapTuple) xtuple)->t_self));
+		Assert(planGen == PLANGEN_PLANNER);
 
-		/*
-		 * insert index entries for tuple
-		 */
-		if (resultRelInfo->ri_NumIndices > 0)
-			ExecInsertIndexTuples(xslot, &(((HeapTuple) xtuple)->t_self), estate, false);
-
-		if (planGen == PLANGEN_PLANNER)
-		{
-			/* AFTER ROW INSERT Triggers */
-			ExecARInsertTriggers(estate, resultRelInfo, tuple);
-		}
+		ExecARInsertTriggers(estate, resultRelInfo, tuple);
 	}
 }
 
@@ -3471,7 +3446,7 @@ ldelete:;
 	{
 		case HeapTupleSelfUpdated:
 			/* already deleted by self; nothing to do */
-		
+
 			/*
 			 * In an scenario in which R(a,b) and S(a,b) have 
 			 *        R               S
@@ -3548,7 +3523,6 @@ ldelete:;
 	 * take care of it later.  We can't delete index tuples immediately
 	 * anyway, since the tuple is still visible to other transactions.
 	 */
-
 
 	if (!(isAORowsTable || isAOColsTable) && planGen == PLANGEN_PLANNER)
 	{
@@ -3753,14 +3727,13 @@ ExecUpdate(TupleTableSlot *slot,
 		   DestReceiver *dest,
 		   EState *estate)
 {
-	void*	tuple;
 	ResultRelInfo *resultRelInfo;
 	Relation	resultRelationDesc;
 	HTSU_Result result;
 	ItemPointerData update_ctid;
 	TransactionId update_xmax = InvalidTransactionId;
-	AOTupleId	aoTupleId = AOTUPLEID_INIT;
-	TupleTableSlot *partslot = NULL;
+	ItemPointerData lastTid;
+	bool		wasHotUpdate;
 
 	/*
 	 * abort the operation if not running transactions
@@ -3779,16 +3752,22 @@ ExecUpdate(TupleTableSlot *slot,
 	bool		rel_is_aocols = RelationIsAoCols(resultRelationDesc);
 	bool		rel_is_external = RelationIsExternal(resultRelationDesc);
 
+	if (rel_is_external &&
+		estate->es_result_partitions &&
+		estate->es_result_partitions->part->parrelid != 0)
+	{
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 errmsg("Update external partitions not supported.")));
+	}
+
 	/*
 	 * get the heap tuple out of the tuple table slot, making sure we have a
 	 * writable copy
 	 */
-	if (rel_is_heap)
-	{
-		partslot = slot;
-		tuple = ExecFetchSlotHeapTuple(partslot);
-	}
-	else if (rel_is_aorows || rel_is_aocols)
+
+
+	if (rel_is_aorows || rel_is_aocols)
 	{
 		/*
 		 * It is necessary to reconstruct a logically compatible tuple to
@@ -3797,38 +3776,12 @@ ExecUpdate(TupleTableSlot *slot,
 		 * columns, and MemTuple cannot deal with cases without converting
 		 * the target list back into the original relation's tuple desc.
 		 */
-		partslot = reconstructMatchingTupleSlot(slot, resultRelInfo);
-
-		/*
-		 * We directly inline toasted columns here as update with toasted columns
-		 * would create two references to the same toasted value.
-		 */
-		tuple = ExecFetchSlotMemTuple(partslot, true);
-	}
-	else if (rel_is_external) 
-	{
-		if (estate->es_result_partitions && 
-			estate->es_result_partitions->part->parrelid != 0)
-		{
-			ereport(ERROR,
-				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-				errmsg("Update external partitions not supported.")));			
-			return;
-		}
-		else
-		{
-			partslot = slot;
-			tuple = ExecFetchSlotHeapTuple(partslot);
-		}
-	}
-	else 
-	{
-		Insist(false);
+		slot = reconstructMatchingTupleSlot(slot, resultRelInfo);
 	}
 
 	/* see if this update would move the tuple to a different partition */
 	if (estate->es_result_partitions)
-		checkPartitionUpdate(estate, partslot, resultRelInfo);
+		checkPartitionUpdate(estate, slot, resultRelInfo);
 
 	/* BEFORE ROW UPDATE Triggers */
 	if (resultRelInfo->ri_TrigDesc &&
@@ -3839,9 +3792,6 @@ ExecUpdate(TupleTableSlot *slot,
 
 		if (slot == NULL)		/* "do nothing" */
 			return;
-
-		/* trigger might have changed tuple */
-		tuple = ExecFetchSlotHeapTuple(slot);
 	}
 
 	/*
@@ -3855,7 +3805,7 @@ ExecUpdate(TupleTableSlot *slot,
 	 */
 lreplace:;
 	if (resultRelationDesc->rd_att->constr)
-		ExecConstraints(resultRelInfo, partslot, estate);
+		ExecConstraints(resultRelInfo, slot, estate);
 
 	if (!GpPersistent_IsPersistentRelation(resultRelationDesc->rd_id))
 	{
@@ -3873,19 +3823,27 @@ lreplace:;
 		 */
 		if (rel_is_heap)
 		{
+			HeapTuple tuple;
+
+			tuple = ExecFetchSlotHeapTuple(slot);
+
 			result = heap_update(resultRelationDesc, tupleid, tuple,
 							 &update_ctid, &update_xmax,
 							 estate->es_output_cid,
 							 estate->es_crosscheck_snapshot,
 							 true /* wait for commit */ );
-		} 
+			lastTid = tuple->t_self;
+			wasHotUpdate = HeapTupleIsHeapOnly(tuple) != 0;
+		}
 		else if (rel_is_aorows)
 		{
+			MemTuple mtuple;
+
 			if (IsXactIsoLevelSerializable)
 			{
 				ereport(ERROR,
 					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-					errmsg("Updates on append-only tables are not supported in serializable transactions.")));			
+					errmsg("Updates on append-only tables are not supported in serializable transactions.")));
 			}
 
 			if (resultRelInfo->ri_updateDesc == NULL)
@@ -3894,8 +3852,13 @@ lreplace:;
 				resultRelInfo->ri_updateDesc = (AppendOnlyUpdateDesc)
 					appendonly_update_init(resultRelationDesc, ActiveSnapshot, resultRelInfo->ri_aosegno);
 			}
+
+			/* appendonly_update() requires that there are no toasted values */
+			mtuple = ExecFetchSlotMemTuple(slot, true);
+
 			result = appendonly_update(resultRelInfo->ri_updateDesc,
-								 tuple, (AOTupleId *) tupleid, &aoTupleId);
+									   mtuple, (AOTupleId *) tupleid, (AOTupleId *) &lastTid);
+			wasHotUpdate = false;
 		}
 		else if (rel_is_aocols)
 		{
@@ -3903,7 +3866,7 @@ lreplace:;
 			{
 				ereport(ERROR,
 					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-					errmsg("Updates on append-only tables are not supported in serializable transactions.")));			
+					errmsg("Updates on append-only tables are not supported in serializable transactions.")));
 			}
 
 			if (resultRelInfo->ri_updateDesc == NULL)
@@ -3913,12 +3876,14 @@ lreplace:;
 					aocs_update_init(resultRelationDesc, resultRelInfo->ri_aosegno);
 			}
 			result = aocs_update(resultRelInfo->ri_updateDesc,
-								 partslot, (AOTupleId *) tupleid, &aoTupleId);
+								 slot, (AOTupleId *) tupleid, (AOTupleId *) &lastTid);
+			wasHotUpdate = false;
 		}
 		else
 		{
-			Assert(!"We should not be here");
+			elog(ERROR, "invalid relation type");
 		}
+
 		switch (result)
 		{
 			case HeapTupleSelfUpdated:
@@ -3946,8 +3911,7 @@ lreplace:;
 					if (!TupIsNull(epqslot))
 					{
 						*tupleid = update_ctid;
-						partslot = ExecFilterJunk(estate->es_junkFilter, epqslot);
-						tuple = ExecFetchSlotHeapTuple(partslot);
+						slot = ExecFilterJunk(estate->es_junkFilter, epqslot);
 						goto lreplace;
 					}
 				}
@@ -3966,10 +3930,11 @@ lreplace:;
 		/*
 		 * Persistent metadata path.
 		 */
-		persistentTuple = heap_copytuple(tuple);
+		persistentTuple = ExecCopySlotHeapTuple(slot);
 		persistentTuple->t_self = *tupleid;
 
 		frozen_heap_inplace_update(resultRelationDesc, persistentTuple);
+		wasHotUpdate = false;
 
 		heap_freetuple(persistentTuple);
 	}
@@ -3993,17 +3958,16 @@ lreplace:;
 	 *
 	 * If it's a HOT update, we mustn't insert new index entries.
 	 */
-	if (rel_is_aorows || rel_is_aocols)
-	{
-		if (resultRelInfo->ri_NumIndices > 0)
-			ExecInsertIndexTuples(partslot, (ItemPointer)&aoTupleId, estate, false);
-	}
-	else
-	{
-		if (resultRelInfo->ri_NumIndices > 0 && !HeapTupleIsHeapOnly((HeapTuple) tuple))
-			ExecInsertIndexTuples(partslot, &(((HeapTuple) tuple)->t_self), estate, false);
+	if (resultRelInfo->ri_NumIndices > 0 && !wasHotUpdate)
+		ExecInsertIndexTuples(slot, &lastTid, estate, false);
 
-		/* AFTER ROW UPDATE Triggers */
+	/* AFTER ROW UPDATE Triggers */
+	if (resultRelInfo->ri_TrigDesc &&
+		resultRelInfo->ri_TrigDesc->n_after_row[TRIGGER_EVENT_UPDATE] > 0 &&
+		rel_is_heap)
+	{
+		HeapTuple tuple = ExecFetchSlotHeapTuple(slot);
+
 		ExecARUpdateTriggers(estate, resultRelInfo, tupleid, tuple);
 	}
 }

src/backend/commands/copy.c
@@ -4228,7 +4228,6 @@ CopyFromDispatch(CopyState cstate)
 static void
 CopyFrom(CopyState cstate)
 {
-	void		*tuple;
 	TupleDesc	tupDesc;
 	Form_pg_attribute *attr;
 	AttrNumber	num_phys_attrs,
@@ -4241,12 +4240,8 @@ CopyFrom(CopyState cstate)
 	int			attnum;
 	int			i;
 	Oid			in_func_oid;
-	Datum		*values = NULL;
-	bool		*nulls = NULL;
 	Datum		*partValues = NULL;
 	bool		*partNulls = NULL;
-	Datum		*baseValues = NULL;
-	bool		*baseNulls = NULL;
 	bool		isnull;
 	ResultRelInfo *resultRelInfo;
 	EState	   *estate = CreateExecutorState(); /* for ExecConstraints() */
@@ -4408,8 +4403,6 @@ CopyFrom(CopyState cstate)
 		CopyFromProcessDataFileHeader(cstate, cdbCopy, &file_has_oids);
 	}
 
-	baseValues = (Datum *) palloc(num_phys_attrs * sizeof(Datum));
-	baseNulls = (bool *) palloc(num_phys_attrs * sizeof(bool));
 	attr_offsets = (int *) palloc(num_phys_attrs * sizeof(int));
 
 	partValues = (Datum *) palloc(attr_count * sizeof(Datum));
@@ -4504,6 +4497,9 @@ PROCESS_SEGMENT_DATA:
 				bool		skip_tuple;
 				Oid			loaded_oid = InvalidOid;
 				char		relstorage;
+				Datum	   *baseValues;
+				bool	   *baseNulls;
+
 				CHECK_FOR_INTERRUPTS();
 
 				/* Reset the per-tuple exprcontext */
@@ -4513,6 +4509,10 @@ PROCESS_SEGMENT_DATA:
 				MemoryContextSwitchTo(GetPerTupleMemoryContext(estate));
 
 				/* Initialize all values for row to NULL */
+				ExecClearTuple(baseSlot);
+				baseValues = slot_get_values(baseSlot);
+				baseNulls = slot_get_isnull(baseSlot);
+
 				MemSet(baseValues, 0, num_phys_attrs * sizeof(Datum));
 				MemSet(baseNulls, true, num_phys_attrs * sizeof(bool));
 				/* reset attribute pointers */
@@ -4822,33 +4822,37 @@ PROCESS_SEGMENT_DATA:
 
 				MemoryContextSwitchTo(GetPerTupleMemoryContext(estate));
 
+				ExecStoreVirtualTuple(baseSlot);
+
 				/*
 				 * And now we can form the input tuple.
+				 *
+				 * The resulting tuple is stored in 'slot'
 				 */
 				if (resultRelInfo->ri_partSlot != NULL)
 				{
 					AttrMap *map = resultRelInfo->ri_partInsertMap;
 					Assert(map != NULL);
 
-
+					slot = resultRelInfo->ri_partSlot;
+					ExecClearTuple(slot);
+					partValues = slot_get_values(resultRelInfo->ri_partSlot);
+					partNulls = slot_get_isnull(resultRelInfo->ri_partSlot);
 					MemSet(partValues, 0, attr_count * sizeof(Datum));
 					MemSet(partNulls, true, attr_count * sizeof(bool));
 
 					reconstructTupleValues(map, baseValues, baseNulls, (int) num_phys_attrs,
 										   partValues, partNulls, (int) attr_count);
-
-					values = partValues;
-					nulls = partNulls;
+					ExecStoreVirtualTuple(slot);
 				}
 				else
 				{
-					values = baseValues;
-					nulls = baseNulls;
+					slot = baseSlot;
 				}
 
 				if (is_check_distkey && distData->p_nattrs > 0)
 				{
-					target_seg = GetTargetSeg(distData, values, nulls);
+					target_seg = GetTargetSeg(distData, slot_get_values(slot), slot_get_isnull(slot));
 					/*check distribution key if COPY FROM ON SEGMENT*/
 					if (GpIdentity.segindex != target_seg)
 						ereport(ERROR,
@@ -4856,31 +4860,6 @@ PROCESS_SEGMENT_DATA:
 								 errmsg("value of distribution key doesn't belong to segment with ID %d, it belongs to segment with ID %d",
 										GpIdentity.segindex, target_seg)));
 				}
-
-				if (relstorage == RELSTORAGE_AOROWS)
-				{
-					/* form a mem tuple */
-					tuple = (MemTuple)
-						memtuple_form_to(resultRelInfo->ri_aoInsertDesc->mt_bind,
-														values, nulls,
-														NULL, NULL, false);
-
-					if (cstate->oids && file_has_oids)
-						MemTupleSetOid(tuple, resultRelInfo->ri_aoInsertDesc->mt_bind, loaded_oid);
-				}
-				else if (relstorage == RELSTORAGE_AOCOLS)
-				{
-                    tuple = NULL;
-				}
-				else
-				{
-					/* form a regular heap tuple */
-					tuple = (HeapTuple) heap_form_tuple(resultRelInfo->ri_RelationDesc->rd_att, values, nulls);
-
-					if (cstate->oids && file_has_oids)
-						HeapTupleSetOid((HeapTuple)tuple, loaded_oid);
-				}
-
 
 				/*
 				 * Triggers and stuff need to be invoked in query context.
@@ -4898,12 +4877,9 @@ PROCESS_SEGMENT_DATA:
 					resultRelInfo->ri_TrigDesc->n_before_row[TRIGGER_EVENT_INSERT] > 0)
 				{
 					HeapTuple	newtuple;
+					HeapTuple	tuple;
 
-					if(relstorage == RELSTORAGE_AOCOLS)
-					{
-						Assert(!tuple);
-						elog(ERROR, "triggers are not supported on tables that use column-oriented storage");
-					}
+					tuple = ExecFetchSlotHeapTuple(slot);
 
 					Assert(resultRelInfo->ri_TrigFunctions != NULL);
 					newtuple = ExecBRInsertTriggers(estate, resultRelInfo, tuple);
@@ -4912,44 +4888,20 @@ PROCESS_SEGMENT_DATA:
 						skip_tuple = true;
 					else if (newtuple != tuple) /* modified by Trigger(s) */
 					{
-						heap_freetuple(tuple);
-						tuple = newtuple;
+						ExecStoreHeapTuple(newtuple, slot, InvalidBuffer, true);
 					}
 				}
 
 				if (!skip_tuple)
 				{
 					char relstorage = RelinfoGetStorage(resultRelInfo);
-					
-					if (resultRelInfo->ri_partSlot != NULL)
-					{
-						Assert(resultRelInfo->ri_partInsertMap != NULL);
-						slot = resultRelInfo->ri_partSlot;
-					}
-					else
-					{
-						slot = baseSlot;
-					}
-
-					if (relstorage != RELSTORAGE_AOCOLS)
-					{
-						/* Place tuple in tuple slot */
-						ExecStoreGenericTuple(tuple, slot, false);
-					}
-
-					else
-					{
-						ExecClearTuple(slot);
-						slot->PRIVATE_tts_values = values;
-						slot->PRIVATE_tts_isnull = nulls;
-						ExecStoreVirtualTuple(slot);
-					}
+					ItemPointerData insertedTid;
 
 					/*
 					 * Check the constraints of the tuple
 					 */
 					if (resultRelInfo->ri_RelationDesc->rd_att->constr)
-							ExecConstraints(resultRelInfo, slot, estate);
+						ExecConstraints(resultRelInfo, slot, estate);
 
 					/*
 					 * OK, store the tuple and create index entries for it
@@ -4957,37 +4909,47 @@ PROCESS_SEGMENT_DATA:
 					if (relstorage == RELSTORAGE_AOROWS)
 					{
 						Oid			tupleOid;
-						AOTupleId	aoTupleId;
-						
+						MemTuple	mtuple;
+
+						mtuple = ExecFetchSlotMemTuple(slot, false);
+
 						/* inserting into an append only relation */
-						appendonly_insert(resultRelInfo->ri_aoInsertDesc, tuple, &tupleOid, &aoTupleId);
-						
-						if (resultRelInfo->ri_NumIndices > 0)
-							ExecInsertIndexTuples(slot, (ItemPointer)&aoTupleId, estate, false);
+						appendonly_insert(resultRelInfo->ri_aoInsertDesc, mtuple, &tupleOid, (AOTupleId *) &insertedTid);
 					}
 					else if (relstorage == RELSTORAGE_AOCOLS)
 					{
-						AOTupleId aoTupleId;
-						
-                        aocs_insert_values(resultRelInfo->ri_aocsInsertDesc, values, nulls, &aoTupleId);
-						if (resultRelInfo->ri_NumIndices > 0)
-							ExecInsertIndexTuples(slot, (ItemPointer)&aoTupleId, estate, false);
+                        aocs_insert(resultRelInfo->ri_aocsInsertDesc, slot);
+						insertedTid = *slot_get_ctid(slot);
 					}
 					else if (relstorage == RELSTORAGE_EXTERNAL)
 					{
+						HeapTuple tuple;
+
+						tuple = ExecFetchSlotHeapTuple(slot);
 						external_insert(resultRelInfo->ri_extInsertDesc, tuple);
+						ItemPointerSetInvalid(&insertedTid);
 					}
 					else
 					{
-						heap_insert(resultRelInfo->ri_RelationDesc, tuple, mycid, use_wal, use_fsm, GetCurrentTransactionId());
+						HeapTuple tuple;
 
-						if (resultRelInfo->ri_NumIndices > 0)
-							ExecInsertIndexTuples(slot, &(((HeapTuple)tuple)->t_self), estate, false);
+						tuple = ExecFetchSlotHeapTuple(slot);
+						heap_insert(resultRelInfo->ri_RelationDesc, tuple, mycid, use_wal, use_fsm, GetCurrentTransactionId());
+						insertedTid = tuple->t_self;
 					}
 
+					if (resultRelInfo->ri_NumIndices > 0)
+						ExecInsertIndexTuples(slot, &insertedTid, estate, false);
 
 					/* AFTER ROW INSERT Triggers */
-					ExecARInsertTriggers(estate, resultRelInfo, tuple);
+					if (resultRelInfo->ri_TrigDesc &&
+						resultRelInfo->ri_TrigDesc->n_after_row[TRIGGER_EVENT_INSERT] > 0)
+					{
+						HeapTuple tuple;
+
+						tuple = ExecFetchSlotHeapTuple(slot);
+						ExecARInsertTriggers(estate, resultRelInfo, tuple);
+					}
 
 					/*
 					 * We count only tuples not suppressed by a BEFORE INSERT trigger;

src/backend/commands/trigger.c
@@ -230,6 +230,21 @@ CreateTrigger(CreateTrigStmt *stmt, Oid constraintOid)
 							NameListToString(stmt->funcname))));
 	}
 
+	/* Check GPDB limitations */
+	if ((RelationIsAoRows(rel) || RelationIsAoCols(rel)) &&
+		TRIGGER_FOR_ROW(tgtype) &&
+		!stmt->isconstraint)
+	{
+		if (TRIGGER_FOR_UPDATE(tgtype))
+			ereport(ERROR,
+					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+					 errmsg("ON UPDATE triggers are not supported on append-only tables")));
+		if (TRIGGER_FOR_DELETE(tgtype))
+			ereport(ERROR,
+					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+					 errmsg("ON DELETE triggers are not supported on append-only tables")));
+	}
+
 	/*
 	 * If the command is a user-entered CREATE CONSTRAINT TRIGGER command that
 	 * references one of the built-in RI_FKey trigger functions, assume it is
@@ -2276,6 +2291,14 @@ GetTupleForTrigger(EState *estate, ResultRelInfo *relinfo,
 	HeapTupleData tuple;
 	HeapTuple	result;
 	Buffer		buffer;
+
+	/* these should be rejected when you try to create such triggers, but let's check */
+	if (RelationIsAoRows(relation) || RelationIsAoCols(relation))
+		elog(ERROR, "UPDATE and DELETE triggers are not supported on append-only tables");
+	if (RelationIsExternal(relation))
+		elog(ERROR, "UPDATE and DELETE triggers are not supported on external tables");
+
+	Assert(RelationIsHeap(relation));
 
 	if (newSlot != NULL)
 	{

hlinnaka/gpdb

commit f5def4508ba1993bbda47018ee376d63c7f21b16

Don't fire INSERT or DELETE triggers on UPDATE.
With ORCA, an UPDATE is actually implemented as an DELETE + INSERT. Don't
fire INSERT or DELETE triggers in that case.

This was broken by commit 740f304eea. I wonder if we should somehow fire
UPDATE triggers in that case, but I don't see any existing code to do that
either.

src/backend/executor/execMain.c
@@ -3159,18 +3159,13 @@ ExecInsert(TupleTableSlot *slot,
 
 	/* BEFORE ROW INSERT Triggers */
 	if (resultRelInfo->ri_TrigDesc &&
-		resultRelInfo->ri_TrigDesc->n_before_row[TRIGGER_EVENT_INSERT] > 0)
+		resultRelInfo->ri_TrigDesc->n_before_row[TRIGGER_EVENT_INSERT] > 0 &&
+		!isUpdate)
 	{
 		HeapTuple	newtuple;
 		HeapTuple	tuple;
 
 		tuple = ExecFetchSlotHeapTuple(slot);
-
-		/*
-		 * Not implemented for ORCA yet. It should fall back to the Postgres planner
-		 * if there are any before-row triggers.
-		 */
-		Assert(planGen == PLANGEN_PLANNER);
 
 		newtuple = ExecBRInsertTriggers(estate, resultRelInfo, tuple);
 
@@ -3284,7 +3279,8 @@ ExecInsert(TupleTableSlot *slot,
 
 	/* AFTER ROW INSERT Triggers */
 	if (resultRelInfo->ri_TrigDesc &&
-		resultRelInfo->ri_TrigDesc->n_after_row[TRIGGER_EVENT_INSERT] > 0)
+		resultRelInfo->ri_TrigDesc->n_after_row[TRIGGER_EVENT_INSERT] > 0 &&
+		!isUpdate)
 	{
 		HeapTuple tuple = ExecFetchSlotHeapTuple(slot);
 

src/test/regress/expected/bfv_dml.out
@@ -225,3 +225,28 @@ set optimizer_trace_fallback = on;
 -- in ORCA currently, so we can use that to trigger fallback.
 update update_pk_test set a=1 where row(1,2) = (SELECT 1, 2);
 ERROR:  Cannot parallelize an UPDATE statement that updates the distribution columns
+reset optimizer_trace_fallback;
+--
+-- Check that INSERT and DELETE triggers don't fire on UPDATE.
+--
+-- It may seem weird how that could happen, but with ORCA, UPDATEs are
+-- implemented as a "split update", which is really a DELETE and an INSERT.
+--
+CREATE TABLE bfv_dml_trigger_test (id int4, t text);
+INSERT INTO bfv_dml_trigger_test VALUES (1, 'foo');
+CREATE OR REPLACE FUNCTION bfv_dml_error_func() RETURNS trigger AS
+$$
+BEGIN
+   RAISE EXCEPTION 'trigger was called!';
+   RETURN NEW;
+END
+$$ LANGUAGE 'plpgsql';
+CREATE TRIGGER before_trigger BEFORE INSERT or DELETE ON bfv_dml_trigger_test
+FOR EACH ROW
+EXECUTE PROCEDURE bfv_dml_error_func();
+CREATE TRIGGER after_trigger AFTER INSERT or DELETE ON bfv_dml_trigger_test
+FOR EACH ROW
+EXECUTE PROCEDURE bfv_dml_error_func();
+UPDATE bfv_dml_trigger_test SET t = 'bar';
+UPDATE bfv_dml_trigger_test SET id = id + 1;
+ERROR:  Cannot parallelize an UPDATE statement that updates the distribution columns

src/test/regress/expected/bfv_dml_optimizer.out
@@ -243,3 +243,27 @@ set optimizer_trace_fallback = on;
 update update_pk_test set a=1 where row(1,2) = (SELECT 1, 2);
 INFO:  GPORCA failed to produce a plan, falling back to planner
 ERROR:  Cannot parallelize an UPDATE statement that updates the distribution columns
+reset optimizer_trace_fallback;
+--
+-- Check that INSERT and DELETE triggers don't fire on UPDATE.
+--
+-- It may seem weird how that could happen, but with ORCA, UPDATEs are
+-- implemented as a "split update", which is really a DELETE and an INSERT.
+--
+CREATE TABLE bfv_dml_trigger_test (id int4, t text);
+INSERT INTO bfv_dml_trigger_test VALUES (1, 'foo');
+CREATE OR REPLACE FUNCTION bfv_dml_error_func() RETURNS trigger AS
+$$
+BEGIN
+   RAISE EXCEPTION 'trigger was called!';
+   RETURN NEW;
+END
+$$ LANGUAGE 'plpgsql';
+CREATE TRIGGER before_trigger BEFORE INSERT or DELETE ON bfv_dml_trigger_test
+FOR EACH ROW
+EXECUTE PROCEDURE bfv_dml_error_func();
+CREATE TRIGGER after_trigger AFTER INSERT or DELETE ON bfv_dml_trigger_test
+FOR EACH ROW
+EXECUTE PROCEDURE bfv_dml_error_func();
+UPDATE bfv_dml_trigger_test SET t = 'bar';
+UPDATE bfv_dml_trigger_test SET id = id + 1;

src/test/regress/sql/bfv_dml.sql
@@ -132,3 +132,35 @@ set optimizer_trace_fallback = on;
 -- Subquery that returns a row rather than a single scalar isn't supported
 -- in ORCA currently, so we can use that to trigger fallback.
 update update_pk_test set a=1 where row(1,2) = (SELECT 1, 2);
+
+reset optimizer_trace_fallback;
+
+
+--
+-- Check that INSERT and DELETE triggers don't fire on UPDATE.
+--
+-- It may seem weird how that could happen, but with ORCA, UPDATEs are
+-- implemented as a "split update", which is really a DELETE and an INSERT.
+--
+CREATE TABLE bfv_dml_trigger_test (id int4, t text);
+
+INSERT INTO bfv_dml_trigger_test VALUES (1, 'foo');
+
+CREATE OR REPLACE FUNCTION bfv_dml_error_func() RETURNS trigger AS
+$$
+BEGIN
+   RAISE EXCEPTION 'trigger was called!';
+   RETURN NEW;
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER before_trigger BEFORE INSERT or DELETE ON bfv_dml_trigger_test
+FOR EACH ROW
+EXECUTE PROCEDURE bfv_dml_error_func();
+
+CREATE TRIGGER after_trigger AFTER INSERT or DELETE ON bfv_dml_trigger_test
+FOR EACH ROW
+EXECUTE PROCEDURE bfv_dml_error_func();
+
+UPDATE bfv_dml_trigger_test SET t = 'bar';
+UPDATE bfv_dml_trigger_test SET id = id + 1;